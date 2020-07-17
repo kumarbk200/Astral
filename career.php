bcf1132c2a3e7161ff978318b69039d8859e2ce9
Bhagyansh Kumar <kumarbk200@yahoo.com>
2020-07-17 14:00:27 +0530

    updated apllication form

--- career.php
+++ career.php
@@ -1,11 +1,83 @@
 <?php include('header.php') ?>
 
+<style>
+  .form-section{
+    padding-top:80px;
+  }
+
+  .form-section form{
+    padding: 30px 0;
+  }
+
+  .apply-div{
+      width:100%;
+      display:flex;
+      justify-content:center;
+      align-items: center;
+  }
+
+  /* .form-section form>div{
+    width: 50%;
+  } */
+
+  .form-section form label{
+    margin-top: 30px;
+    background: none;
+  }
+
+  .form-section form input{
+    border:none;
+    border-bottom: 1px solid grey;
+    width: 100%;
+    background: none;
+  }
+  
+
+  .form-section form select{
+    width: 100%;
+    border: 1px solid grey;
+    background: none;
+  }
+
+  .form-section form textarea{
+    width:100%;
+    margin-top: 40px;
+    background: none;
+  }
+
+  .form-section #formModal form  .modal-footer #submit-btn{
+    padding: 15px 30px;
+    margin-top: 30px;
+    width: 30%;
+    font-size: 1rem; 
+    background: #0067da;
+    border:none;
+  }
+
+  .form-section form #file{
+    border:none;
+    background: none;
+  }
+
+  @media (max-width:767px){
+    .form-section form>div{
+      width: 100%;
+    }
+    .form-section form input,.form-section form select,.form-section form textarea{
+      width: 100%;
+    }
+  }
+
+
+</style>
+
+
 <!-- Page Title START -->
 <div class="page-title-section" style="background-image: url('img/banner/careerbg.png');">
 	<div class="black-overlay-70"></div>
 	<div class="container">
-		<h1>Work With Us</h1><br>
-		<p >Specialised Sterile Formulation for Strategic Alliance.</p>
+		<h1>Join Our Team</h1><br>
+		<p >Work Culture Driven by Collaboration and Meritocracy</p>
 	</div>
 </div>
 <!-- Page Title END -->
@@ -16,69 +88,70 @@
       <div class="col-md-6 career-text">
 
          <h2 class="textcolor"> Career Opportunities:</h2><hr class="title1-hr"><br>
-<p>As we grow manifolds in the Manufacturing sector, so does our need for hiring the best of talents. Among many things, Astral Steritech is defined by work culture where we are driven by our company values of creating value and serving the humankind. At the same time, we encourage each and every team member to dream and assist in creating a shared future.</p>
-          </div>
-          <div class="col-md-6">
+          <p>As our company continues to grow, we’re always on the look-out for professional, talented and ambitious people that can help take our business to the next level and service our clients’ needs. Among many things, Astral SteriTech is defined by work culture where we are driven by our company values of creating value and serving the humankind. At the same time, we encourage each and every team member to dream and assist in creating a shared future.</p>
+          <p>We strive to employ dedicated individuals who have a strong work ethic, are goal-oriented and take pride in accomplishments, have a collaborative approach to team-work, and excel in problem-solving in a deadline-oriented work environment. We are seeking highly motivated, independent yet team-oriented candidates with an enthusiastic and positive attitude, excellent multi-tasking skills and willingness to excel in a fast-paced, client-oriented, team environment.</p> 
+        </div>
+          <div class="col-md-6 d-flex">
           <img class="img-responsive" src="img/ASPL-Logo-with-Moto-700x700.png" width="70%;">
           </div>
-       <div class="col-md-12 career-text">
-         <h2 class="textcolor"> Why now?</h2><hr class="title-hr"><br>
-<p>The world will forever be in a turmoil. It is our duty to create order out of chaos. Astral Steritech provides that order and helps you achieve your best potential. Check out the current opportunities and apply.<br><br></p>
-          </div>
+        <!-- <div class="col-md-12 career-text">
+          <h2 class="textcolor"> Why now?</h2><hr class="title-hr"><br>
+          <p>The world will forever be in a turmoil. It is our duty to create order out of chaos. Astral Steritech provides that order and helps you achieve your best potential. Check out the current opportunities and apply.<br><br></p>
+        </div> -->
           <div class="col-md-12">
-              <div class="web-table">
-          <div class="row">
-    <div class="col-12">
-      <table class="table table-bordered">
-        <thead>
-          <tr>
-            <th scope="col">No.</th>
-            <th scope="col">Position Name</th>
-            <th scope="col">Education</th>
-            <th scope="col">Experince (in Years)</th>
-            <th scope="col">Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th scope="row">1</th>
-            <td>Job 1</td>
-            <td>B.Ed</td>
-            <td>2</td>
-            <td>
-       <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a>
-              <!-- <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">2</th>
-            <td>Job 2</td>
-            <td>Msc</td>
-            <td>3</td>
-            <td>
-            <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a>
-             <!--  <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
-            </td>
-          </tr>
-          <tr>
-            <th scope="row">3</th>
-            <td>Job 3</td>
-            <td>Bsc</td>
-            <td>1</td>
-            <td>
-            <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a>
-              <!-- <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
-            <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
-            </td>
-          </tr>
-        </tbody>
-      </table>
+                        <!-- <div class="web-table">
+                    <div class="row">
+              <div class="col-12">
+                <table class="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th scope="col">No.</th>
+                      <th scope="col">Position Name</th>
+                      <th scope="col">Education</th>
+                          <th scope="col">Experince (in Years)</th>
+                    <th scope="col">Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Job 1</td>
+                    <td>B.Ed</td>
+                    <td>2</td>
+                    <td>
+              <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a> -->
+                      <!-- <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
+                    <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
+                    <!-- </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Job 2</td>
+                    <td>Msc</td>
+                    <td>3</td>
+                    <td>
+                    <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a> -->
+                    <!--  <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
+                    <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
+                    <!-- </td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td>Job 3</td>
+                  <td>Bsc</td>
+                  <td>1</td>
+                  <td>
+                  <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a> -->
+                    <!-- <button type="button" class="btn btn-success"><i class="fas fa-edit"></i></button>
+                  <button type="button" class="btn btn-danger"><i class="far fa-trash-alt"></i></button> -->
+                  <!-- </td>
+                </tr>
+              </tbody>
+            </table>
     </div>
   </div></div>
               
-              <div class="mobile-table">
+      <div class="mobile-table">
 <div class="row card-mobile">
 <div class="col-12 mb-2">
 <div class="card" style="width: 100%;">
@@ -90,9 +163,9 @@
    <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a>
 </a>
   </div>
-</div>
+</div> -->
 
-</div></div>
+<!-- </div></div>
 <div class="row card-mobile">
 <div class="col-12 mb-2">
 <div class="card" style="width: 100%;">
@@ -116,14 +189,130 @@
     <a href="#" class="card-link">              
 <a href="#" class="button-primary hvr-sweep-to-right button-sm">Apply Now</a>
 </a></div>
-</div>
+</div> -->
 
-</div></div>
+<!-- </div></div> -->
+
+<!-- </div> -->
+
+
+
+
 
 </div>
-          </div>
         
-        </div> 
+</div>
+
+<section class="form-section">
+  <div class="container">
+      <div class="apply-div">
+        <div>
+            <h2 class="textcolor"> Apply for a position at Astral SteriTech</h2><hr class="title1-hr ml-auto mr-auto"><br>
+        </div>
+      </div>
+      <div class="apply-div">
+        <a href="#" class="button-primary hvr-sweep-to-right button-sm" data-target="#formModal" data-toggle="modal">Apply Now</a>
+      </div>
+      
+      <div class="modal" id="formModal">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2 class="textcolor" style="width: 100%; text-align:center;">Application Form <span><br></span><hr class="title1-hr ml-auto mr-auto"></h2>
+                        <button type="button" class="close" data-dismiss="modal"> &times; </button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="">
+                            <div class="form-group">
+                                <label for="fname">First name:</label><br>
+                                <input type="text" id="fname" name="fname" required><br>
+                            </div>
+                            <div class="form-group">
+                                <label for="lname">Last name:</label><br>
+                                <input type="text" id="lname" name="lname" required><br>
+                            </div>
+                            <div class="form-group">
+                                <label for="email">Email Id:</label><br>
+                                <input type="email" id="email" name="email" required><br>
+                            </div>
+                            <div class="form-group">
+                                <label for="depOfInterest">Department of Interest:</label><br>
+                                <select id="interests" name="interests" required>
+                                    <option value="Production">Production</option>
+                                    <option value="Quality Assurance">Quality Assurance</option>
+                                    <option value="Quality Control">Quality Control</option>
+                                    <option value="Warehouse/Stores">Warehouse/Stores</option>
+                                    <option value=" PPIC/Purchase"> PPIC/Purchase</option>
+                                    <option value="Import & Export">Import & Export</option>
+                                    <option value="Business Development">Business Development</option>
+                                    <option value="HR & Admin">HR & Admin</option>
+                                    <option value="IT"> IT</option>
+                                    <option value="Engineering">Engineering</option>
+                                    <option value="EHS">EHS</option>
+                                    <option value="Other">Other</option>
+                                    
+                                </select><br>
+                            </div>
+                            <div class="form-group">
+                                <label for="currPosition">Current Position:</label><br>
+                                <input type="text" id="currposition" name="currposition" required><br>
+                            </div>
+                            <div class="form-group">
+                                <textarea rows="4" name="message" form="" placeholder=" Comments"></textarea><br>
+                            </div>
+                            <div class="form-group">
+                                <label for="upload">Upload CV :</label><br>
+                                <input id="file" type="file" name="file" required><br>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer justify-content-center">
+                        <button type="submit"><a href="#" class="button-primary hvr-sweep-to-right button-sm" data-target="#formModal" data-toggle="modal">Submit</a></button>
+                        <!-- <input id="submit-btn" class="button-primary hvr-sweep-to-right button-sm" type="submit" value="Apply Now"> -->
+                    </div>
+                </div>
+            </div>
+      </div>
+      
+      <!-- <h2 class="textcolor">Apply Now</h2><hr class="title1-hr"><br> -->
+      <!-- <form method="post" action="" enctype="multipart/form-data">
+        <div>
+        <label for="fname">First name:</label><br>
+        <input type="text" id="fname" name="fname" required><br>
+        <label for="lname">Last name:</label><br>
+        <input type="text" id="lname" name="lname" required><br>
+        <label for="email">Email Id:</label><br>
+        <input type="email" id="email" name="email" required><br>
+        <label for="depOfInterest">Department of Interest:</label><br>
+        <select id="interests" name="interests" required>
+            <option value="Production">Production</option>
+            <option value="Quality Assurance">Quality Assurance</option>
+            <option value="Quality Control">Quality Control</option>
+            <option value="Warehouse/Stores">Warehouse/Stores</option>
+            <option value=" PPIC/Purchase"> PPIC/Purchase</option>
+            <option value="Import & Export">Import & Export</option>
+            <option value="Business Development">Business Development</option>
+            <option value="HR & Admin">HR & Admin</option>
+            <option value="IT"> IT</option>
+            <option value="Engineering">Engineering</option>
+            <option value="EHS">EHS</option>
+            <option value="Other">Other</option>
+            
+          </select><br>
+          <label for="currPosition">Current Position:</label><br>
+          <input type="text" id="currposition" name="currposition" required><br>
+          <textarea rows="4" name="message" form="" placeholder=" Comments"></textarea><br>
+          <label for="upload">Upload CV :</label><br>
+          <input id="file" type="file" name="file" required><br>
+
+          <div>
+         
+          <input id="submit-btn" class="button-primary hvr-sweep-to-right button-sm" type="submit" value="Apply Now">
+        </div>
+      </form> -->
+  </div>
+
+</section> 
         
     </div>
     </div>
